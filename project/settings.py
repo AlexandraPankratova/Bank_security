716fd60b8dd29fd0835452e1e3e15528e88f6a2b
AlexandraPankratova <ablackcat@yandex.ru>
2021-04-19 16:14:13 +0300

    Upload all DB info from .env

--- project/settings.py
+++ project/settings.py
@@ -7,12 +7,12 @@ env.read_env()
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': env('HOST'),
-        'PORT': '5434',
-        'NAME': 'checkpoint',
-        'USER': 'guard',
-        'PASSWORD': env('PASSWORD'),
+        'ENGINE': env('DB_ENGINE'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
     }
 }
 
